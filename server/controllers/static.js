115ea7a71a81c1c61ad65a54604f47bb24a72dae
dickeyxxx <jeff@dickeyxxx.com>
2015-10-11 13:39:30 -0700

    hook into node

--- server/controllers/static.js
+++ server/controllers/static.js
@@ -4,6 +4,7 @@ let express = require('express');
 let router  = express.Router();
 
 router.use(express.static(`${__dirname}/../../public`));
+router.use(express.static(`${__dirname}/../../bower_components`));
 
 router.get('/', function (req, res) {
   res.render('index.html.ejs');
